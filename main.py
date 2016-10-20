473b7c44f27595c99a277771890360412164dbe5
Seunghun Lee <waydi1@gmail.com>
2016-10-19 23:14:23 -0700

    Add blog entry model

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
-# coding=utf-8
+import os
 
 import webapp2
 import jinja2
 
-import os
+from google.appengine.ext import db
 
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
 
 class BlogHandler(webapp2.RequestHandler):
-    template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-    jinja_env = jinja2.Environment(
-            loader = jinja2.FileSystemLoader(template_dir),
-            autoescape = True
-            )
-
     def write(self, *a, **kw):
         self.response.write(*a, **kw)
 
     def render_str(self, template, **params):
-        t = self.jinja_env.get_template(template)
+        t = jinja_env.get_template(template)
         return t.render(params)
 
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+class Entry(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
 class MainPage(BlogHandler):
     def get(self):
-        self.render('base.html')
+        entries = db.GqlQuery('SELECT * FROM Entry ORDER BY created DESC')
+        self.render('base.html', entries = entries)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
